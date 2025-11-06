03d35af7c16f8376a7ceaf43586c4e4b6acd04e7
Vivek Nerkar <nvivek742@gmail.com>
2025-11-06 22:34:56 +0530

    added migrations

--- Havenza.Core/Entities/Product.cs
+++ Havenza.Core/Entities/Product.cs
@@ -13,12 +13,13 @@ namespace Havenza.Core.Entities
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
-        public decimal Price { get; set; }
+        public double Price { get; set; }
+
         public int CategoryId { get; set; } // Foreign key for the Category (will link to Category entity)
 
         // Navigation property for the product's category
         // This creates a relationship to the Category entity
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
         public string? ImageUrl { get; set; }         // URL/path to the product image
         public int Stock { get; set; }
 
