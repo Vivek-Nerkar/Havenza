8f83ce235e0e42f30cbef07a1ef2a6ed4ab43cbb
Vivek Nerkar <nvivek742@gmail.com>
2025-12-26 14:50:55 +0530

    Initial push

---
+++ LogiTrack/LogiTrack/Models/InventoryItem.cs
@@ -0,0 +1,14 @@
+﻿namespace LogiTrack.Models
+{
+    public class InventoryItem
+    {
+        public int ItemId { get; set; }
+        public string? Item { get; set; }
+        public int Quantity { get; set; }
+        public string? Location { get; set; }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Item} | Quantity: {Quantity} | Location: {Location}");
+        }
+    } 
+}
---
+++ LogiTrack/LogiTrack/Models/Order.cs
@@ -0,0 +1,39 @@
+﻿namespace LogiTrack.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string? CustomerName { get; set; }   
+        public DateTime DataPlaced { get; set; }
+        public List<InventoryItem>? Items { get; set; }
+
+        public void AddItem(InventoryItem item)
+        {
+            if (Items == null)
+            {
+                Items = new List<InventoryItem>();
+            }
+            Items.Add(item);
+        }
+        public void RemoveItem(InventoryItem item)
+        {
+            Items?.Remove(item);
+        }
+        public void GetOrderDetails()
+        {
+            Console.WriteLine($"Order ID: {OrderId} | Customer: {CustomerName} | Date Placed: {DataPlaced}");
+            if (Items != null && Items.Count > 0)
+            {
+                Console.WriteLine("Items in Order:");
+                foreach (var item in Items)
+                {
+                    item.DisplayInfo();
+                }
+            }
+            else
+            {
+                Console.WriteLine("No items in this order.");
+            }
+        }
+    }
+}
